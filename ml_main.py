13ce45280bf642f082d22963ad2c9ea6d64a2d96
Erfan6561 <153428984+Erfan6561@users.noreply.github.com>
2024-01-07 21:28:50 +0300

    Add files via upload

--- ml_main.py
+++ ml_main.py
@@ -3,24 +3,28 @@ import io
 import streamlit as st
 from transformers import pipeline
 
+import streamlit as st
+import pandas as pd
 
 st.title('Тест на токсика')
+
+
+text = st.text_input('Enter your text', '')
+
 
 # Обученая модель для распознавания "токсичности" в тексте
 clf = pipeline(
     task = 'sentiment-analysis', 
     model = 'SkolkovoInstitute/russian_toxicity_classifier')
 
-# Сам текст
-text = ['Какой замечательный новый год!',
-        'Я не собираюсь терпеть эту чушь с её стороны, уф.']
+
 
 def data(text):
     for row in text:
         yield row
 
 for out in clf(data(text)):
-    print(out)
+    print(st.write(out))
 
 #вывод
 #{'label': 'neutral', 'score': 0.9872767329216003}
