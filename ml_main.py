7394dac1a405ccb2b02cfcadf13231f0baaf387c
Erfan6561 <153428984+Erfan6561@users.noreply.github.com>
2024-01-07 19:55:12 +0300

    Add files via upload

--- ml_main.py
+++ ml_main.py
@@ -1,6 +1,22 @@
+# Импорт библиотеки
 from transformers import pipeline
 
-classifier = pipeline("sentiment-analysis",   
-                      "blanchefort/rubert-base-cased-sentiment")
+# Обученая модель для распознавания "токсичности" в тексте
+clf = pipeline(
+    task = 'sentiment-analysis', 
+    model = 'SkolkovoInstitute/russian_toxicity_classifier')
 
-classifier("Я обожаю инженерию машинного обучения!")
+# Сам текст
+text = ['Какой замечательный новый год!',
+        'Я не собираюсь терпеть эту чушь с её стороны, уф.']
+
+def data(text):
+    for row in text:
+        yield row
+
+for out in clf(data(text)):
+    print(out)
+
+#вывод
+#{'label': 'neutral', 'score': 0.9872767329216003}
+#{'label': 'toxic', 'score': 0.985331654548645}
